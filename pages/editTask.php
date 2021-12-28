a2b4c491e4b7dbdde3b28fcf817a10ff0a0da381
Bikram48 <chandbikram001@gmail.com>
2021-12-28 12:59:27 +0545

    add - form on edit page

--- pages/editTask.php
+++ pages/editTask.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title></title>
 </head>
 <body>
-   
+   <div class="container">
+       <h2>Update Task Page</h2>
+       <form>
+           <div class="row">
+               <div class="col-xl-4">
+                    <input type="text" name="taskname">
+               </div>
+               <div class="col-sm-4 col-xl-4">
+                   <input type="submit" value="UPDATE" name="update_btn">
+               </div>
+           </div>
+           <p id="error_display"></p>
+       </form>
+   </div>
 </body>
 </html>
