7c29fe9ab8f40ce6138f6ece355dad8cd4da9162
Bikram48 <chandbikram001@gmail.com>
2021-12-28 12:01:15 +0545

    feat: add form that takes tasks info

--- pages/home.php
+++ pages/home.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    
+    <div class="container">
+        <h2>To-Do Task App</h2>
+        <form>
+            <div class="row">
+                <div class="col-xl-4">
+                    <input type="text" placeholder="Enter task name" name="task_name"/>
+                </div>
+                <div class="col-xl-4">
+                    <input type="submit"/>
+                </div>
+            </div>
+            <p></p>
+        </form>
+    </div>
 </body>
 </html>
