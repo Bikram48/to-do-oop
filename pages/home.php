c3e25cc3039e17e6f8208e98e720fa1b66282285
Bikram48 <chandbikram001@gmail.com>
2021-12-28 12:20:17 +0545

    feat: style home page

--- pages/home.php
+++ pages/home.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"/>
+    <link rel="stylesheet" href="../style/home.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
     <title>Document</title>
 </head>
 <body>
@@ -20,14 +21,14 @@
                     <input type="submit"/>
                 </div>
             </div>
-            <p></p>
+            <p id="error_display"></p>
         </form>
         <form>
             <div class="form-group">
                 <div class="row">
                     <div class="col-sm-offest-2">
                         <div class="checkbox">
-                            <input type="checkbox" name="ids[]">Taskname1
+                            <input type="checkbox" name="ids[]"><span id="taskname">Taskname1</span>
                         </div>
                     </div>
                     <div class="col-xl-2">
